d7a74ddbd3bad8dedaf0f46e84d794f113204776
Kerryn Song <kerrynsong@Kerryns-MBP.NetComm.Wireless>
2018-05-17 19:49:18 +1200

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAMGxj5EH5ybAkSkFL7DaTKi84CgZChG18",
+    authDomain: "p3-7860d.firebaseapp.com",
+    databaseURL: "https://p3-7860d.firebaseio.com",
+    projectId: "p3-7860d",
+    storageBucket: "",
+    messagingSenderId: "806105097238"
+};
+firebase.initializeApp(config);
