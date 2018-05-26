44dfaad5d1eafcd91a861ad8430b32eb456cbe5c
Kerryn Song <kerrynsong@Kerryns-MBP.NetComm.Wireless>
2018-05-27 00:22:10 +1200

    test failed

--- js/script.js
+++ js/script.js
@@ -14,20 +14,10 @@
 var rootRef = firebase.database().ref();
 var tracksRef = rootRef.child('Tracks');
 
-tracksRef.orderByChild("Size").equalTo("large").on("child_added", function (snapshot) {
-    var newTrack = snapshot.val();
-    $("#lrgResults").append("<div>" + newTrack.Track + "<br><br> Distance: " + newTrack.Distance + "<br> Time: Approximately " + newTrack.Time + "<br> Suitable for " + newTrack.Size + " dogs of " + newTrack.Fitness + " fitness </div>");
-});
-
 tracksRef.orderByChild("Size").equalTo("medium").on("child_added", function (snapshot) {
     var newTrack = snapshot.val();
     
-    $("#medResults").append("<div>" + newTrack.Track + "<br><br> Distance: " + newTrack.Distance + "<br> Time: Approximately " + newTrack.Time + "<br> Suitable for " + newTrack.Size + " dogs of " + newTrack.Fitness + " fitness </div>");
-});
-
-tracksRef.orderByChild("Size").equalTo("small").on("child_added", function (snapshot) {
-    var newTrack = snapshot.val();
-    $("#smlResults").append("<div>" + newTrack.Track + "<br><br> Distance: " + newTrack.Distance + "<br> Time: Approximately " + newTrack.Time + "<br> Suitable for " + newTrack.Size + " dogs of " + newTrack.Fitness + " fitness </div>");
+    $("#medResults").append("<a href=" + newTrack.URL + "<div>" + newTrack.Track + "<br><br> Distance: " + newTrack.Distance + "<br> Time: Approximately " + newTrack.Time + "<br> Suitable for " + newTrack.Size + " dogs of " + newTrack.Fitness + " fitness </div></a>");
 });
 
 function showPage() {
