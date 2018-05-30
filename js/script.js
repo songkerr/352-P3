fbc8281f651988549adad9a8d981a85ee34925da
Kerryn Song <kerrynsong@Kerryns-MacBook-Pro.local>
2018-05-30 18:23:52 +1200

    made track titles bold

--- js/script.js
+++ js/script.js
@@ -17,7 +17,13 @@ var tracksRef = rootRef.child('Tracks');
 tracksRef.orderByChild("Size").equalTo("medium").on("child_added", function (snapshot) {
     var newTrack = snapshot.val();
     
-    $("#medResults").append("<div id='infodiv'> <a href=" + newTrack.URL + " <p> " + newTrack.Track + "<br><br> Distance: " + newTrack.Distance + "<br> Time: Approximately " + newTrack.Time + "<br> Suitable for " + newTrack.Size + " dogs of " + newTrack.Fitness + " fitness</p></a></div>");
+    $("#medResults").append("<div class='infodiv'> <a href=" + newTrack.URL + " <p> " + newTrack.Track + "<br><br> Distance: " + newTrack.Distance + "<br> Time: Approximately " + newTrack.Time + "<br> Suitable for " + newTrack.Size + " dogs of " + newTrack.Fitness + " fitness</p></a></div>");
+});
+
+tracksRef.orderByChild("Size").equalTo("large").on("child_added", function (snapshot) {
+    var newTrack = snapshot.val();
+    
+    $("#lrgResults").append("<div class='infodiv'> <a href=" + newTrack.URL + " <p> " + newTrack.Track + "</p><p> Distance: " + newTrack.Distance + "<br> Time: Approximately " + newTrack.Time + "<br> Suitable for " + newTrack.Size + " dogs of " + newTrack.Fitness + " fitness</p></a></div>");
 });
 
 function showPage() {
